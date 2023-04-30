0788bd910f4933073d31fe5daed8aedcae5bb90c
kannan6633 <kokila8754366529@gmail.com>
2023-04-30 12:32:54 +0530

    verify

---
+++ src/test/java/com/data/Test.java
@@ -0,0 +1,16 @@
+package com.data;
+
+public class Test {
+	public static void main(String[] args) {
+
+		int a=50;
+		int d =50;
+		int c=50;
+
+		System.out.println("branch");
+		System.out.println("1");
+		System.out.println("1");
+		System.out.println("1");
+	}
+
+}
